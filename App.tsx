f8391be0b3e4dbf7fb09d80ef6d65202fc671f34
iktanim <iktanim1995@gmail.com>
2022-12-02 19:21:51 +0600

    Header section done

--- App.tsx
+++ App.tsx
@@ -1,8 +1,10 @@
-import { View, Text, SafeAreaView, FlatList, TouchableOpacity, Platform } from 'react-native'
+import { View, Text, SafeAreaView, FlatList, TouchableOpacity, Platform, PixelRatio } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import CircleProgressComponent from './CircleProgressComponent'
 import Toast from 'react-native-toast-message'
 import ToastConfigCustomComponent from './src/components/ToastConfigCustomComponent'
+import { StyleSheet } from 'react-native'
+import { ScrollView } from 'react-native'
 const backupData = [
   {name:'goal1', image:require('./resources/images/Goal1.png'), fromStatus: "inProgress", toStatus: "completed"},
   {name:'goal2', image:require('./resources/images/Goal2.png'), fromStatus: "locked", toStatus: "inProgress"},
@@ -10,30 +12,41 @@ const backupData = [
   {name:'goal4', image:require('./resources/images/Goal4.png'), fromStatus: "locked", toStatus: "locked"},
   {name:'PopQuiz', image:require('./resources/images/PopQuiz.png'), fromStatus: "locked", toStatus: "locked"},
 ]
+const size = 80;
 
-export default function App() {
+const App = () => {
   const [data, setData] = useState(backupData)
   const [uniqueValue, setUniqueValue] = useState(1)
 
   useEffect(() => {
     
   }, [])
-  
-
-  return (
+  // const size = PixelRatio.getPixelSizeForLayoutSize(40);
+  return (  
     <SafeAreaView key={uniqueValue}>
-      <FlatList
-        style={{marginTop:20}}
-        data={data}
-        renderItem={({item, index})=>
-          <View style={{marginHorizontal:4}}>
-            {/* <CircleProgressComponent data={item} animate={item.fromStatus != item.toStatus}/> */}
-            <CircleProgressComponent data={item}/>
+      <View style={{flexDirection:'row'}}>
+        <View style={{height:size, width: size, marginLeft: 12,marginRight: 4,marginTop:4}}>
+          <View 
+            style={styles.headerContainer}>
+            <Text style={styles.headerText}>GETTING STARTED</Text>
           </View>
-        }
-        horizontal
-        keyExtractor={(item, index)=>index.toString()}
-      />
+          <View style={styles.footerContainer}>
+            <Text style={styles.headerText}>1/5</Text>
+          </View>
+        </View>
+        <FlatList
+          data={data}
+          contentContainerStyle={{paddingRight:12}}
+          renderItem={({item, index})=>
+            <View style={{marginHorizontal:4}}>
+              {/* <CircleProgressComponent data={item} animate={item.fromStatus != item.toStatus}/> */}
+              <CircleProgressComponent data={item}/>
+            </View>
+          }
+          horizontal
+          keyExtractor={(item, index)=>index.toString()}
+        />
+      </View>
       <TouchableOpacity onPress={() => {
         // setData(backupData); setUniqueValue(prevUniqueValue => prevUniqueValue+1)
         Toast.show({
@@ -47,3 +60,34 @@ export default function App() {
     </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  headerContainer: {
+    width: size-8,
+    height: size-8,
+    backgroundColor: '#FFF4E2',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 20,
+  },
+  headerText: {
+    fontSize: 12,
+    color: '#E6B270',
+    fontWeight: '800',
+  },
+  footerContainer: {
+    position: 'absolute',
+    bottom: 0,
+    right: -6,
+    backgroundColor: '#FFF4E2',
+    height: 24,
+    width: 32,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth:1,
+    borderColor:'#fff',
+    borderRadius:10, 
+    marginTop:-12,
+  },
+});
+export default App;
